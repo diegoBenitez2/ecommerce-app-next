cf83e7527dd15bcb4393616767f5b017aee91496
diegoFer <45721607+diegoBenitez2@users.noreply.github.com>
2023-09-27 22:43:30 -0500

    chore: create collapse component

--- src/components/molecules/Collapse/Collapse.jsx
+++ src/components/molecules/Collapse/Collapse.jsx
@@ -1,35 +1,24 @@
 import { useState } from 'react';
 import { FiChevronDown } from 'react-icons/fi';
-import  Checkbox from '@atoms/Checkbox/Checkbox';
 import cn from 'classnames';
 
 export const Collapse = ({
-  title='Product type',
-  items=['furniture', 'homeware', 'sofas'],
-  onChange }) => {
+  label,
+  content,
+}) => {
 
   const [collapse, setCollapse] = useState(false);
 
   return (
-    <div className="px-4 py-3 text-left text-dark-primary border-2 border-x-white border-y-light-gray relative">
-      <div className="flex items-center justify-between w-full">
-        <span className="font-clash text-dark-primary mr-3">{title}</span>
+    <div className={cn("px-4 text-left text-dark-primary border-2 border-x-white border-y-light-gray relative overflow-hidden transition-all duration-[500ms] ease-in-out", [collapse ? 'h-[52px] mt-0': 'h-fit'])}>
+      <div className={cn("flex items-center justify-between w-full py-3 border-white")}>
+        <span className="font-clash text-dark-primary mr-3">{label}</span>
         <FiChevronDown className={cn("w-[20px] h-[20px] text-dark-primary cr-pointer transition transform ease duration-500", {'rotate-180': collapse})}
         onClick={() => setCollapse(!collapse)}/>
       </div>
-      <ul className={cn("p-0 border-white overflow-hidden transition-all ease-in-out ", [collapse ? 'h-[0] mt-0': 'mt-3 h-[auto] border-2 border-t-light-gray  '])}>
-        { items && items.map((item) => (
-          <li
-            className="py-2 text-left text-dark-primary flex items-center"
-            key={item}>
-            <Checkbox
-              label={item}
-              value={item}
-              className="mr-3"
-              onchange={onChange} />
-          </li>
-        ))}
-      </ul>
+      <div className={cn("p-0 border-white", { "border-2 border-t-light-gray": !collapse })}>
+        {content}
+      </div>
     </div>
   );
 };
